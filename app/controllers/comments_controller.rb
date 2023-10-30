7a91c67e6b7e41896384b534be58a9a452dbe964
namtay071 <hongdiem31120@gmail.com>
2023-10-30 17:40:08 +0700

    comment-1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,16 @@ class CommentsController < ApplicationController
         @comment.destroy
         redirect_to post_path(@post)
     end
+    def update
+        @comment = @post.comments.find(params[:id])
+        respond_to do |format|
+            if @comment.update(comment_params)
+                format.html { redirect_to post_url(@post), notice: 'Comment has been updated' }
+            else
+                format.html { redirect_to post_url(@post), alert: 'Comment has not been updated' }
+            end
+        end
+    end
     private
     def set_post
         @post = Post.find(params[:post_id])
